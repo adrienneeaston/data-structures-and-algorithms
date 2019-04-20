e9ba1105b2f9ed5a58065df435e399d30481a22e
Adrienne <adrienneeaston@gmail.com>
2019-04-19 17:56:57 -0700

    question 2 passing

--- 301/string-methods/challenges-05.test.js
+++ 301/string-methods/challenges-05.test.js
@@ -33,8 +33,9 @@ Write a function name wordsToCharList that, given a string as input, returns a n
 For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
-const wordsToCharList = (arr) => {
-  // Solution code here...
+const wordsToCharList = (str) => {
+  let arr = str.split('');
+  return arr;
 };
 
 
