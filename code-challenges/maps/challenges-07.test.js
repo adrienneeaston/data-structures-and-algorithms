b6293457393ecc78b95ef6409a82dbcf5b178fed
Adrienne <adrienneeaston@gmail.com>
2019-04-26 23:50:32 -0700

    question 1 passes

--- code-challenges/maps/challenges-07.test.js
+++ code-challenges/maps/challenges-07.test.js
@@ -11,7 +11,8 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
+  let newArray = arr.map((value, index) => 2 ** value);
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
