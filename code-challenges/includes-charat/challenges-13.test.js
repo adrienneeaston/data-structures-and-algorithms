2e45c071295e68c0f646daca02c6f2bbd4104d7d
Adrienne <adrienneeaston@gmail.com>
2019-05-18 00:11:30 -0700

    question 1 passing

--- code-challenges/includes-charat/challenges-13.test.js
+++ code-challenges/includes-charat/challenges-13.test.js
@@ -8,9 +8,8 @@ Write a function named firstLetters that takes in an array of strings and return
 For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 'w', 'w', ':']
 ------------------------------------------------------------------------------------------------ */
 
-const firstLetters = (arr) => {
-  // Solution code here...
-};
+const firstLetters = (arr) => arr.map(banana => banana.charAt(0));
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
