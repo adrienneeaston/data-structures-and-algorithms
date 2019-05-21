7b5cbaacf8dc0c90f6e74b090ad8e57aafb21c2a
Adrienne <adrienneeaston@gmail.com>
2019-05-20 17:59:20 -0700

    question 1 passing

--- code-challenges/combination/challenges-14.test.js
+++ code-challenges/combination/challenges-14.test.js
@@ -9,7 +9,7 @@ For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyv
 ------------------------------------------------------------------------------------------------ */
 
 const toTitleCase = (arr) => {
-  // Solution code here...
+  return arr.map(str => str.charAt(0).toUpperCase() + str.substring(1, str.length));
 };
 
 /* ------------------------------------------------------------------------------------------------
