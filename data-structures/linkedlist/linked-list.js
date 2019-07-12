a6bf90f5d0d0dfa4529ff0e93a5b1f3aa49d06c6
Adrienne <adrienneeaston@gmail.com>
2019-07-11 20:19:47 -0700

    kFromTheEnd written

--- data-structures/linkedlist/linked-list.js
+++ data-structures/linkedlist/linked-list.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { ModuleMap } from "jest-haste-map";
+
 // import { tsExternalModuleReference, exportAllDeclaration } from "@babel/types";
 
 class Node {
@@ -17,18 +19,14 @@ class LinkedList{
   }
 
   append(value) {
-    if(!this.head) {
+    if(!this.head && !this.tail) {
       this.head = new Node(value);
+      this.tail = this.head;
       return;
     }
-
-    let current = this.head;
-    while(current.next) {
-      current = current.next;
-    }
-
-    current.next = Node(value);
-    this.tail = current.next;
+    this.tail.next = new Node(value);
+    this.tail = this.tail.next;
+    this.length++;
   }
 
   insert(value) {
@@ -90,34 +88,25 @@ class LinkedList{
     return dataArray.toString();
   };
   // refactor to do...while
-}
 
+  kFromTheEnd(k) {
+    if(k >= list.length || k < 0 || typeof k !== number)
+    {throw error};
+    if(k === 0) {return this.list.tail};
+    let i = this.length - k -1;
+    let current = this.head;
+    while(i < 0) {
+      current = current.head;
+      i -= 1;
+    }
+    return current.value;
+  };
+};
 
+// let list = new LinkedList;
+// list.append(1);
+// list.append(2);
+// list.append(3);
+// console.log(list.head);
 
-
-// describe('linkedlist', () => {
-//   test('blah should do blah', () => {
-//     let list = new LinkedList();
-//     // add some values to the list
-//     expect(list.head.value).toEqual(1);
-//     expect(list.head.next.value).toEqual(2);
-//     expect(list.head.next.next.value).toEqual(3);
-//     expect(list.head.next.next.next.value).toEqual(4);
-//   });
-
-//   test('list.isEmpty(), should be true for an empty list', () => {
-//     let list = new LinkedList();
-//     expect(list.isEmpty()).toEqual(true);
-//   });
-
-//   test('list.isEmpty() should be false for an unempty list', () => {
-//     let list = new LinkedList();
-//     list.append(1);
-//     expect(list.isEmpty()).toEqual(false);
-//   });
-
-//   test('list.size() should be 0 for an empty list', () => {
-//     let list = new LinkedList();
-//     expect(list.size()).toEqual(0);
-//   });
-// });
+module.exports = LinkedList;
