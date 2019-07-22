1f0036f668b20416c06d1bfb4a36cf0f9dfee97b
Adrienne <adrienneeaston@gmail.com>
2019-07-21 17:10:59 -0700

    tests scaffolded

--- data-structures/_tests_/stacks-queues.test.js
+++ data-structures/_tests_/stacks-queues.test.js
@@ -2,30 +2,68 @@
 
 const LinkedList = require('../linkedlist/linked-list.js');
 
-describe('Stacks and Queues', () => {
+describe('Stacks', () => {
   it('can successfully instantiate an empty stack', () => {
     let stack = new Stack();
     expect(stack.top).toBeNull();
     expect(stack instanceof Stack).toBeTruthy();
   });
 
-// Can successfully push onto a stack
-// Can successfully push multiple values onto a stack
-// Can successfully pop off the stack
-// Can successfully empty a stack after multiple pops
-// Can successfully peek the next item on the stack
+  it('can successfully push onto a stack', () => {
+    let stack = new Stack();
 
+  });
+
+  it('can successfully push multiple values onto a stack', () => {
+    let stack = new Stack();
+    
+  });
+
+  it('can successfully pop off the stack', () => {
+    let stack = new Stack();
+    
+  });
+
+  it('can successfully empty a stack after multiple pops', () => {
+    let stack = new Stack();
+    
+  });
+
+  it('can successfully peek the next item on the stack', () => {
+    let stack = new Stack();
+    
+  });
+});
+
+describe('Queues', () => {
   it('can successfully instantiate an empty queue', () => {
     let queue = new Queue();
     expect(queue.top).toBeNull();
     expect(queue instanceof Queue).toBeTruthy();
   });
 
-// Can successfully instantiate an empty stack
-// Can successfully enqueue into a queue
-// Can successfully enqueue multiple values into a queue
-// Can successfully dequeue out of a queue the expected value
-// Can successfully peek into a queue, seeing the expected value
-// Can successfully empty a queue after multiple dequeues
+  it('can successfully enqueue into a queue', () => {
+    let queue = new Queue();
 
+  });
+
+  it('can successfully enqueue multiple values into a queue', () => {
+    let queue = new Queue();
+
+  });
+
+  it('can successfully dequeue out of a queue the expected value', () => {
+    let queue = new Queue();
+
+  });
+
+  it('can successfully peek into a queue, seeing the expected value', () => {
+    let queue = new Queue();
+
+  });
+
+  it('can successfully empty a queue after multiple dequeues', () => {
+    let queue = new Queue();
+
+  });
 });
