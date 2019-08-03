93abc581200c678765ddd799a4a217496d6370fa
Adrienne <adrienneeaston@gmail.com>
2019-08-03 12:47:50 -0700

    code start

---
+++ data-structures/tree/tree.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  
+
+  preOrder() {
+
+  }
+
+  inOrder() {
+
+  }
+
+  postOrder() {
+
+  }
+
+  levelOrder() {
+
+  }
+
+}
