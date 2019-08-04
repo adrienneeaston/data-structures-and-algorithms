25d859d38a87a6555bf43d981376e61ee634bb2e
Adrienne <adrienneeaston@gmail.com>
2019-08-03 22:52:02 -0700

    code start

--- data-structures/tree/tree.js
+++ data-structures/tree/tree.js
@@ -13,21 +13,62 @@ class BinaryTree {
     this.root = root;
   }
 
-  
-
   preOrder() {
+    let results = [];
 
+    let _walk = (node) => {
+      results.push(node.value);
+      if(node.left) { _walk(node.left); }
+      if(node.right) { _walk(node.right); }
+    };
+
+    _walk(this.root);
+
+    return results;
   }
 
   inOrder() {
+    let results = [];
 
+    let _walk = (node) => {
+      results.push(node.value);
+      if(node.left) { _walk(node.left); }
+      if(node.right) { _walk(node.right); }
+    };
+
+    _walk(this.root);
+
+    return results;
   }
 
   postOrder() {
+    let results = [];
 
+    let _walk = (node) => {
+      results.push(node.value);
+      if(node.left) { _walk(node.left); }
+      if(node.right) { _walk(node.right); }
+    };
+
+    _walk(this.root);
+
+    return results;
   }
 
   levelOrder() {
+    let results = [];
+    let nodeQueue = [];
+
+    nodeQueue.push(this.root);
+
+    while(nodeQueue.length) {
+      let current = nodeQueue.shift();
+      results.push(current.value);
+      if(current.left) {nodeQueue.push(current.left);}
+      if(current.right) {nodeQueue.push(current.right);}
+    }
+
+    return results;
 
   }
 
