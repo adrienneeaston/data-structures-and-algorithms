87291582a34212488ff4c916ba7c44a1a7db1ba9
Adrienne <adrienneeaston@gmail.com>
2019-08-05 21:53:25 -0700

    work start

---
+++ data-structures/tree/fizzbuzztree/fizz-buzz-tree.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+    root ? this.root = new Node(root) : this.root = root;
+  }
+
+  fizzBuzzTree(value) {
+
+    let _walk = (node) => {
+
+      if(node.left) {_walk(node.left);}
+      if(node.right) {_walk(node.right);}
+      if(node.value % 3 === 0 && node.value % 5 === 0)
+    };
+
+    _walk(this.root);
+
+    return results;
+};
+
+module.exports = FizzBuzzTree;
