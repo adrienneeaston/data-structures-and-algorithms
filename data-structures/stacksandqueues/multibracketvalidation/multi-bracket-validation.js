8fbef9e3d039b949a73873a50fbaff441da59ee7
Adrienne <adrienneeaston@gmail.com>
2019-08-09 17:01:47 -0700

    2 of 3 tests passing

--- data-structures/stacksandqueues/multibracketvalidation/multi-bracket-validation.js
+++ data-structures/stacksandqueues/multibracketvalidation/multi-bracket-validation.js
@@ -1,32 +1,35 @@
 'use strict';
  
-const Stacks = require('../stacks.js');
+const Stack = require('../stacks.js');
 
-multiBracketValidation() {
-  let mbv = function(input) {
-    if(typeofinput !== string) {return false;}
-  let validation = newStack();
-    for(let i = 0; i < string.length; i++) {
-      if(i === '(' || i === '[' || i === '{') {
-        stack.push(i);
-        if(i === ')') {
-          if(stack.push() === '(') {
-            stack.pop();
-          } else {return false;}
-        }
-        if(i === ']') {
-          if(stack.push() === '[') {
-            stack.pop();
-          } else {return false;}
-        }
-        if(i === '}') {
-          if(stack.push() === '{') {
-            stack.pop();
-          } else {return false;}
-        }
-        if(stack.top) {return true;} 
-        else {return false;}  
-      }   
+let multiBracketValidation = (input) => {
+  if(typeof input !== 'string') {return false;}
+
+  let stack = new Stack();
+
+  for(let i = 0; i < input.length; i++) {
+    if(i === '(' || i === '[' || i === '{') {
+      stack.push(i);
+      if(i === ')') {
+        if(stack.push() === '(') {
+          stack.pop();
+        } else {return false;}
+      }
+      if(i === ']') {
+        if(stack.push() === '[') {
+          stack.pop();
+        } else {return false;}
+      }
+      if(i === '}') {
+        if(stack.push() === '{') {
+          stack.pop();
+        } else {return false;}
+      }
     }
   }
+
+  if(stack.top) {return false;} 
+  else {return true;}  
 };
+
+module.exports = multiBracketValidation;
