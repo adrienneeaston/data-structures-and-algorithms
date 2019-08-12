507ed6cae257fbb99dc60d0968d135112d2943da
Adrienne <adrienneeaston@gmail.com>
2019-08-11 18:33:32 -0700

    mixedbracketvalidation complete, all tests passing

--- data-structures/stacksandqueues/multibracketvalidation/multi-bracket-validation.js
+++ data-structures/stacksandqueues/multibracketvalidation/multi-bracket-validation.js
@@ -4,31 +4,30 @@ const Stack = require('../stacks.js');
 
 let multiBracketValidation = (input) => {
   if(typeof input !== 'string') {return false;}
-
   let stack = new Stack();
 
   for(let i = 0; i < input.length; i++) {
-    if(i === '(' || i === '[' || i === '{') {
-      stack.push(i);
-      if(i === ')') {
-        if(stack.push() === '(') {
-          stack.pop();
-        } else {return false;}
-      }
-      if(i === ']') {
-        if(stack.push() === '[') {
-          stack.pop();
-        } else {return false;}
-      }
-      if(i === '}') {
-        if(stack.push() === '{') {
-          stack.pop();
-        } else {return false;}
-      }
+    if(input[i] === '(' || input[i] === '[' || input[i] === '{') {
+      stack.push(input[i]);
+    }
+    if(input[i] === ')') {
+      if(stack.peek() === '(') {
+        stack.pop();
+      } else {return false;}
+    }
+    if(input[i] === ']') {
+      if(stack.peek() === '[') {
+        stack.pop();
+      } else {return false;}
+    }
+    if(input[i] === '}') {
+      if(stack.peek() === '{') {
+        stack.pop();
+      } else {return false;}
     }
   }
 
-  if(stack.top) {return false;} 
+  if(stack.peek()) {return false;} 
   else {return true;}  
 };
 
