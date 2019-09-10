110ed81e6b2668a09c307679264fbb2a7b76b6a4
Adrienne <adrienneeaston@gmail.com>
2019-09-09 20:05:29 -0700

    more work, test complete but not passing

--- sorts/mergesort/merge-sort.js
+++ sorts/mergesort/merge-sort.js
@@ -40,8 +40,8 @@ function mergeSort(arr) {
   let n = arr.length;
   if (n > 1) {
     let mid = n/2;
-    let left = arr[0...mid];
-    let right = arr[mid...n];
+    let left = arr[0, ...mid];
+    let right = arr[mid;...n];
     mergeSort(left);
     mergeSort(right);
     mergeSort(left, right, arr);
@@ -52,7 +52,7 @@ function merge(left, right, arr) {
   let i = 0;
   let j = 0;
   let k = 0;
-  
+
   while(i < left.length && j < right.length) {
     i++;
   } else { 
