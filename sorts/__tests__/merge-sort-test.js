afd7a65a1a53b429698c0512684b592b300da54e
Adrienne <adrienneeaston@gmail.com>
2019-09-09 19:31:58 -0700

    start on algorithm

---
+++ sorts/__tests__/merge-sort-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const insertionSort = require('../mergesort/merge-sort.js');
+
+describe('merge sort', () => {
+  it('Can successfully sort an array', () => {
+    let array = [5, 8, 3, 13, 11, 5];
+    expect(mergeSort(array)).toEqual([3, 5, 5, 8, 11, 13])
+  });
+});
