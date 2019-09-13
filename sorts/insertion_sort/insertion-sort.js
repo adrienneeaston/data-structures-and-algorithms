65121aa9422e68372aa45fe4246ad87d3af9dda1
Adrienne <adrienneeaston@gmail.com>
2019-09-12 21:46:31 -0700

    some cleanup

--- sorts/insertion_sort/insertion-sort.js
+++ sorts/insertion_sort/insertion-sort.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// pseudocode
-
-// InsertionSort(int[] arr)
-  
-// FOR i = 1 to arr.length
-
-//   int j <-- i - 1
-//   int temp <-- arr[i]
-  
-//   WHILE j >= 0 AND temp < arr[j]
-//     arr[j + 1] <-- arr[j]
-//     j <-- j - 1
-    
-//   arr[j + 1] <-- temp
-
-
-
 function insertionSort(arr) {
   for(let i = 0; i < arr.length; i++) {
     let temp = arr[i];
