70c9f9b3b11d40fcb3a5efdf14af3dbd54a97a25
Adrienne <adrienneeaston@gmail.com>
2019-09-14 09:23:45 -0700

    cleaning up all sorts

--- sorts/quicksort/quick-sort.js
+++ sorts/quicksort/quick-sort.js
@@ -1,36 +1,36 @@
 'use strict';
 
 function quickSort(arr, left, right) {
-  var len = arr.length, 
-  pivot,
-  position;
+  let pivot;
+  let position;
 
- if(left < right) {
-   pivot = right;
-   position = partition(arr, pivot, left, right);
-   
-  quickSort(arr, left, position - 1);
-  quickSort(arr, position + 1, right);
- }
- return arr;
+  if(left < right) {
+    pivot = right;
+    position = partition(arr, pivot, left, right);
+    
+    quickSort(arr, left, position - 1);
+    quickSort(arr, position + 1, right);
+  }
+  return arr;
 };
 
 function partition(arr, pivot, left, right) {
-  var pivotValue = arr[pivot],
-      position = left;
+  let pivotValue = arr[pivot];
+  let position = left;
 
-  for(var i = left; i < right; i++) {
-   if(arr[i] < pivotValue) {
-     swap(arr, i, position);
-     position++;
-   }
- }
- swap(arr, right, position);
- return position;
+  for(let i = left; i < right; i++) {
+    if(arr[i] < pivotValue) {
+      swap(arr, i, position);
+      position++;
+    }
+  }
+
+  swap(arr, right, position);
+  return position;
 };
        
 function swap(arr, i, j){
-  var temp = arr[i];
+  let temp = arr[i];
   arr[i] = arr[j];
   arr[j] = temp;
 };
